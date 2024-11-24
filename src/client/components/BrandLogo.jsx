b758f366afb7953bf9a1a904378e41b5b0bd9940
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-11-24 22:07:18 +0100

    Adds new logo

--- src/client/components/BrandLogo.jsx
+++ src/client/components/BrandLogo.jsx
@@ -1,9 +1,431 @@
-import { Box } from 'lucide-react';
-
 const BrandLogo = () => {
   return (
     <div className='col-start-1 row-start-1 mr-auto flex h-[52px] items-center px-2 shadow'>
-      <Box className='h-8 w-8 text-emerald-600' />
+      <svg
+        width='50'
+        height='35'
+        viewBox='0 0 206 143'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M105.722 62.8088L153.501 41.9601L152.632 98.4254L105.722 118.405V62.8088Z'
+          fill='url(#paint0_linear_0_1)'
+        />
+        <path
+          d='M155.238 45.4348L203.016 24.5861L202.148 81.0514L155.238 101.031V45.4348Z'
+          fill='url(#paint1_linear_0_1)'
+        />
+        <path
+          d='M87.4797 18.5052L149.157 38.4852L103.116 58.4653L42.3074 38.4852L87.4797 18.5052Z'
+          fill='#0ECD92'
+        />
+        <path
+          d='M139.601 4.60608L199.542 23.7174L157.844 41.96L105.722 19.3739L139.601 4.60608Z'
+          fill='#0ECD92'
+        />
+        <path
+          d='M37.9639 41.9601L100.51 62.8088V118.405L37.9639 97.5567V41.9601Z'
+          fill='#059669'
+        />
+        <path
+          d='M203.885 24.5861L203.885 78.4453'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M203.885 24.5861L155.238 45.4348'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M139.601 2L103.116 17.6365'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M87.4797 15.8991L35.3578 38.4853'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M151.763 40.2227L103.116 61.0714'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M155.238 38.4853L155.238 99.294'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M103.116 61.0714L103.116 121.88'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M35.3578 38.4853L35.3578 99.294'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M103.116 121.88L203.885 78.4453'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M35.3578 99.294L102.815 121.88'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M35.3578 38.4853L102.815 61.0714'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M87.4797 15.8991L154.937 38.4853'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M139.601 2L203.584 22.8487'
+          stroke='black'
+          stroke-width='3'
+          stroke-linecap='round'
+        />
+        <path
+          d='M117.015 116.668V78.4453'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M110.066 55.8592L49.257 35.0105'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M118.753 52.3844L57.944 31.5357'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M127.44 48.9096L64.8935 26.3235'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M136.127 45.4348L73.5805 22.8487'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M143.076 41.9601L80.5301 19.3739'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M170.875 38.4853L108.328 15.8991'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M177.824 35.0105L115.278 12.4244'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M186.511 31.5357L123.965 8.94959'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M169.137 92.3445V57.5966'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M143.076 99.294L143.076 66.2836'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M195.198 76.7079L195.198 45.4348'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M117.015 69.7583L143.076 57.5966'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M169.137 50.647L195.198 38.4853'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M117.015 78.4453L143.076 66.2836'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M169.137 57.5966L195.198 45.4349'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M193.461 28.0609L130.915 5.4748'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M42.3074 101.031L42.3074 41.9601'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M49.257 104.506L49.257 45.4348'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M56.2066 104.506L56.2066 45.4348'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M64.8936 107.981L64.8936 48.9096'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M71.8431 111.456L71.8431 52.3844'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M78.7927 113.193L78.7927 54.1218'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M87.4797 116.668L87.4797 57.5966'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <path
+          d='M94.4292 118.405L94.4292 59.334'
+          stroke='black'
+          stroke-linecap='round'
+        />
+        <g filter='url(#filter0_i_0_1)'>
+          <path
+            d='M143.076 66.7469V100.163L117.884 88.8697V78.4453L143.076 66.7469Z'
+            fill='black'
+          />
+        </g>
+        <g filter='url(#filter1_i_0_1)'>
+          <path
+            d='M144.18 97.5835C145.033 97.8863 145.077 99.0756 144.249 99.4408L117.015 111.456V87.9376L144.18 97.5835Z'
+            fill='black'
+            fill-opacity='0.52'
+          />
+          <path
+            d='M193.6 73.7447C195.099 74.1911 195.858 76.7774 194.46 77.485L168.539 88.8086V66.2836L193.6 73.7447Z'
+            fill='black'
+            fill-opacity='0.52'
+          />
+        </g>
+        <g filter='url(#filter2_i_0_1)'>
+          <path
+            d='M195.198 45.4348V77.5766L169.137 67.5577V57.1333L195.198 45.4348Z'
+            fill='black'
+          />
+        </g>
+        <g filter='url(#filter3_d_0_1)'>
+          <path
+            d='M33.6205 99.2941L106.591 122.749L76.1866 134.911L4.95301 110.587L33.6205 99.2941Z'
+            fill='#010101'
+          />
+        </g>
+        <defs>
+          <filter
+            id='filter0_i_0_1'
+            x='117.884'
+            y='66.7469'
+            width='25.1922'
+            height='37.4159'
+            filterUnits='userSpaceOnUse'
+            color-interpolation-filters='sRGB'
+          >
+            <feFlood flood-opacity='0' result='BackgroundImageFix' />
+            <feBlend
+              mode='normal'
+              in='SourceGraphic'
+              in2='BackgroundImageFix'
+              result='shape'
+            />
+            <feColorMatrix
+              in='SourceAlpha'
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+              result='hardAlpha'
+            />
+            <feOffset dy='4' />
+            <feGaussianBlur stdDeviation='2' />
+            <feComposite in2='hardAlpha' operator='arithmetic' k2='-1' k3='1' />
+            <feColorMatrix
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+            />
+            <feBlend
+              mode='normal'
+              in2='shape'
+              result='effect1_innerShadow_0_1'
+            />
+          </filter>
+          <filter
+            id='filter1_i_0_1'
+            x='117.015'
+            y='66.2836'
+            width='78.1827'
+            height='49.1722'
+            filterUnits='userSpaceOnUse'
+            color-interpolation-filters='sRGB'
+          >
+            <feFlood flood-opacity='0' result='BackgroundImageFix' />
+            <feBlend
+              mode='normal'
+              in='SourceGraphic'
+              in2='BackgroundImageFix'
+              result='shape'
+            />
+            <feColorMatrix
+              in='SourceAlpha'
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+              result='hardAlpha'
+            />
+            <feOffset dy='4' />
+            <feGaussianBlur stdDeviation='2' />
+            <feComposite in2='hardAlpha' operator='arithmetic' k2='-1' k3='1' />
+            <feColorMatrix
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+            />
+            <feBlend
+              mode='normal'
+              in2='shape'
+              result='effect1_innerShadow_0_1'
+            />
+          </filter>
+          <filter
+            id='filter2_i_0_1'
+            x='169.137'
+            y='45.4348'
+            width='26.0609'
+            height='36.1418'
+            filterUnits='userSpaceOnUse'
+            color-interpolation-filters='sRGB'
+          >
+            <feFlood flood-opacity='0' result='BackgroundImageFix' />
+            <feBlend
+              mode='normal'
+              in='SourceGraphic'
+              in2='BackgroundImageFix'
+              result='shape'
+            />
+            <feColorMatrix
+              in='SourceAlpha'
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+              result='hardAlpha'
+            />
+            <feOffset dy='4' />
+            <feGaussianBlur stdDeviation='2' />
+            <feComposite in2='hardAlpha' operator='arithmetic' k2='-1' k3='1' />
+            <feColorMatrix
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+            />
+            <feBlend
+              mode='normal'
+              in2='shape'
+              result='effect1_innerShadow_0_1'
+            />
+          </filter>
+          <filter
+            id='filter3_d_0_1'
+            x='0.953011'
+            y='99.2941'
+            width='109.638'
+            height='43.6166'
+            filterUnits='userSpaceOnUse'
+            color-interpolation-filters='sRGB'
+          >
+            <feFlood flood-opacity='0' result='BackgroundImageFix' />
+            <feColorMatrix
+              in='SourceAlpha'
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+              result='hardAlpha'
+            />
+            <feOffset dy='4' />
+            <feGaussianBlur stdDeviation='2' />
+            <feComposite in2='hardAlpha' operator='out' />
+            <feColorMatrix
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+            />
+            <feBlend
+              mode='normal'
+              in2='BackgroundImageFix'
+              result='effect1_dropShadow_0_1'
+            />
+            <feBlend
+              mode='normal'
+              in='SourceGraphic'
+              in2='effect1_dropShadow_0_1'
+              result='shape'
+            />
+          </filter>
+          <linearGradient
+            id='paint0_linear_0_1'
+            x1='153.063'
+            y1='41.96'
+            x2='106.16'
+            y2='118.405'
+            gradientUnits='userSpaceOnUse'
+          >
+            <stop stop-color='#0ECD92' />
+            <stop offset='1' stop-color='#023022' />
+          </linearGradient>
+          <linearGradient
+            id='paint1_linear_0_1'
+            x1='203.016'
+            y1='28.851'
+            x2='155.238'
+            y2='96.7666'
+            gradientUnits='userSpaceOnUse'
+          >
+            <stop stop-color='#0ECD92' />
+            <stop offset='1' stop-color='#023022' />
+          </linearGradient>
+        </defs>
+      </svg>
       <span className='ml-2 text-xl font-bold text-gray-900'>InventoryPro</span>
     </div>
   );
