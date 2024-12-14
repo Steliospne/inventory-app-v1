335ee6ddd6260542650c9d282d64e6791866dcbb
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-12-14 22:00:18 +0100

    Migrating files to typescript

--- src/client/components/Input.tsx
+++ src/client/components/Input.tsx
@@ -2,7 +2,20 @@
  * Creates an Input element with label
  */
 
-const Input = ({
+import React from 'react';
+
+interface InputProps {
+  type?: string;
+  id?: string;
+  name?: string;
+  required?: boolean;
+  autoComplete?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  LabelText?: string;
+  value?: string | number;
+}
+
+const Input: React.FC<InputProps> = ({
   type = 'text',
   id,
   name = id,
@@ -22,7 +35,7 @@ const Input = ({
         name={name && name}
         id={id}
         autoComplete={autoComplete}
-        onChange={onChange && onChange}
+        onChange={onChange}
         required={required}
         value={value}
         className='mt-4 h-10 rounded-lg border border-emerald-300 p-2 px-4 py-5 shadow-sm focus:border-emerald-500 focus:ring focus:ring-emerald-200 focus:ring-opacity-50'
